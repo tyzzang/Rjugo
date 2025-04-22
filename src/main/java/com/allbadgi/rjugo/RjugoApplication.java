df5c3fb7136d3c36ba8ad2ff6f20e9bb08731c67
tyzzang <132876590+tyzzang@users.noreply.github.com>
2025-04-22 21:34:40 +0900

    Initial commit

---
+++ src/main/java/com/allbadgi/rjugo/RjugoApplication.java
@@ -0,0 +1,13 @@
+package com.allbadgi.rjugo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RjugoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RjugoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allbadgi/rjugo/RjugoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allbadgi.rjugo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RjugoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
